e2cdc0559b5d93cf7d04755540ca156c06d72732
Bogdan Pankevych <>
2024-09-27 16:55:29 +0200

    initial commit

---
+++ MVC-MyToDos/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  MVC-MyToDos
+//
+//  Created by admin on 27.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
